76582529a6868c75302d2d93b9a1e631bcf9c3df
Ege Hidayet Koca <kocaege777@gmail.com>
2026-01-02 13:28:00 +0300

    feat: implement main workflow

--- src/implementation-roadmap.js
+++ src/implementation-roadmap.js
@@ -13,3 +13,8 @@ export const stage3 = {
   "date": "2025-12-22",
   "message": "feat: configure persistence or state layer"
 };
+
+export const stage4 = {
+  "date": "2026-01-02",
+  "message": "feat: implement main workflow"
+};
